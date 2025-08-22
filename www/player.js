902cdea950662be33fd151c4f0ddc5800e40d872
Ricardo <flugel.ricardo77@gmail.com>
2025-08-22 20:48:00 +0200

    setting up the boundaries

--- www/player.js
+++ www/player.js
@@ -1,12 +1,13 @@
 // move with keyboard
 let player = document.getElementById('player');
-let posX = 100;
-let posY = 100;
+let posX = 500;
+let posY = 500;
 let speed = 1;
 
 let keyPressed = new Set();
 addEventListener('keydown', (key) => {
     keyPressed.add(key.key.toLowerCase());
+
 })
 
 addEventListener('keyup', (key) => {
@@ -14,17 +15,27 @@ addEventListener('keyup', (key) => {
 })
 
 
-let move_player = () => {
-    if (keyPressed.has('w')) posY -= speed;
-    if (keyPressed.has('s')) posY += speed;
-    if (keyPressed.has('a')) posX -= speed;
-    if (keyPressed.has('d')) posX += speed;
+let movePlayer = () => {
+    if (posY < 1000 && posY > -1000){
+        if (keyPressed.has('w')) posY -= speed;
+        if (keyPressed.has('s')) posY += speed;
+    }
+
+    if (posX < 1000 && posX > -1000){
+        if (keyPressed.has('a')) posX -= speed;
+        if (keyPressed.has('d')) posX += speed;
+    }
 
     player.style.top = posY + "px";
     player.style.left = posX + "px";
 
-    requestAnimationFrame(move_player)
+    requestAnimationFrame(movePlayer) // calls the movePlayer method 60 times per second.
 
 }
 
-move_player();
+movePlayer();
+
+addEventListener('click', (event) => {
+    console.log("X : " + event.clientX)
+    console.log("Y : " + event.clientY)
+})
