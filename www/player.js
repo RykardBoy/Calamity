058b1a0517fc57018619fd756069ca725aad30c1
Ricardo <flugel.ricardo77@gmail.com>
2025-08-22 20:23:24 +0200

    movement of the player

--- www/player.js
+++ www/player.js
@@ -1,12 +1,30 @@
 // move with keyboard
-let move_player = (player) => {
-    addEventListener('keyup', (key) => {
-        console.log(key.key)
-        switch (key.key){
-            case 'w': player.clientX + 1;
+let player = document.getElementById('player');
+let posX = 100;
+let posY = 100;
+let speed = 1;
 
-        }
-    })
+let keyPressed = new Set();
+addEventListener('keydown', (key) => {
+    keyPressed.add(key.key.toLowerCase());
+})
+
+addEventListener('keyup', (key) => {
+    keyPressed.delete(key.key.toLowerCase())
+})
+
+
+let move_player = () => {
+    if (keyPressed.has('w')) posY -= speed;
+    if (keyPressed.has('s')) posY += speed;
+    if (keyPressed.has('a')) posX -= speed;
+    if (keyPressed.has('d')) posX += speed;
+
+    player.style.top = posY + "px";
+    player.style.left = posX + "px";
+
+    requestAnimationFrame(move_player)
+
 }
 
-move_player(document.getElementById('player'));
+move_player();
